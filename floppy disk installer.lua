bbd74fc6900e44907446bb0901ba2dda48f2a58b
Braxtonship <braxya14@hotmail.com>
2026-05-16 17:26:53 +1000

    Implement BAC package manager installation on floppy disk
    Added installation logic for BAC package manager and disk ejection.

---
+++ floppy disk installer.lua
@@ -0,0 +1,25 @@
+-- install this on a floppy disk only
+term.clear()
+print("Would you like to install BAC package manager?")
+term.setTextColour(colours.red)
+print("Y/N")
+term.setTextColour(colours.white)
+local input = read()
+if input == "y" then
+    print("installing...")
+    term.setTextColour(colours.yellow)
+    shell.run("wget", "https://raw.githubusercontent.com/Braxtonship/braxtons-cc-tweaked-programs/refs/heads/main/BAC-package-manager.lua", "bac")
+    term.setTextColour(colours.green)
+    print("installation complete!")
+    term.setTextColour(colours.pink)
+    print("ejecting disk(s)...")
+    term.setTextColour(colours.white)
+    disk.eject("top")
+    disk.eject("bottom")
+    disk.eject("left")
+    disk.eject("right")
+    disk.eject("front")
+    disk.eject("back")
+else
+    print("installation aborted...")
+end
