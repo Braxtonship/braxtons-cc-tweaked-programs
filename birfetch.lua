0aa7f632e864ebe3a94826ca4627cb98d72fa1e1
Braxtonship <braxya14@hotmail.com>
2026-05-16 16:53:45 +1000

    Fix syntax error in computer label check

--- birfetch.lua
+++ birfetch.lua
@@ -4,7 +4,7 @@ print("birfetch:")
 print("-----------------")
 print("running: ", os.version())
 print("computer id: ", os.getComputerID())
-if os.getComputerLabel()) == nil then
+if os.getComputerLabel() == nil then
     print("computer label: ", os.getComputerLabel())
 else
     print("computer label: n/a")
