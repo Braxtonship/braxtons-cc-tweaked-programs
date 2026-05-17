0b97517ad5b555448ba212aaf50e3d01e1c3c014
Braxtonship <braxya14@hotmail.com>
2026-05-17 10:55:39 +1000

    Update BACPAX-PM.lua

--- BACPAX-PM.lua
+++ BACPAX-PM.lua
@@ -418,9 +418,9 @@ if args[1] == "install" then
             installPackage() --install the package
             return
         else
-            print('the package: "',args[2],'" could not be found.')
-            print("did you type it correctly? (use -l or --packages)")
-            return
         end
     end
+    print('the package: "',args[2],'" could not be found.')
+    print("did you type it correctly? (use -l or --packages)")
+    return
 end
