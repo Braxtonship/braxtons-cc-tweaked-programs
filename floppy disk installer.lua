1b5ebffb43b86f66708d89b3402d63e68b5d6612
Braxtonship <braxya14@hotmail.com>
2026-05-21 17:53:06 +1000

    Update floppy disk installer.lua

--- floppy disk installer.lua
+++ floppy disk installer.lua
@@ -4,8 +4,8 @@ print("Would you like to install BAC package manager?")
 term.setTextColour(colours.red)
 print("Y/N")
 term.setTextColour(colours.white)
-local input = read()
-if input == "y" then
+local args = { ... }
+if args[1] == "-y" then
     print("installing...")
     term.setTextColour(colours.yellow)
     shell.run("wget", "https://raw.githubusercontent.com/Braxtonship/braxtons-cc-tweaked-programs/refs/heads/main/BACPAX-PM.lua", "bacpax")
@@ -20,6 +20,30 @@ if input == "y" then
     disk.eject("right")
     disk.eject("front")
     disk.eject("back")
+elseif args[1] == "-s"
+    print("installing...")
+    term.setTextColour(colours.yellow)
+    shell.run("wget", "https://raw.githubusercontent.com/Braxtonship/braxtons-cc-tweaked-programs/refs/heads/main/BACPAX-PM.lua", "bacpax")
+    term.setTextColour(colours.green)
+    print("installation complete!")
 else
-    print("installation aborted...")
+    local input = read()
+    if input == "y" then
+        print("installing...")
+        term.setTextColour(colours.yellow)
+        shell.run("wget", "https://raw.githubusercontent.com/Braxtonship/braxtons-cc-tweaked-programs/refs/heads/main/BACPAX-PM.lua", "bacpax")
+        term.setTextColour(colours.green)
+        print("installation complete!")
+        term.setTextColour(colours.pink)
+        print("ejecting disk(s)...")
+        term.setTextColour(colours.white)
+        disk.eject("top")
+        disk.eject("bottom")
+        disk.eject("left")
+        disk.eject("right")
+        disk.eject("front")
+        disk.eject("back")
+    else
+        print("installation aborted...")
+    end
 end
