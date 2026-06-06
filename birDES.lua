c81f2bebaa82634769316a6176bb5e57cd37e1c8
Braxtonship <braxya14@hotmail.com>
2026-06-06 12:03:41 +1000

    Update birDES.lua

--- birDES.lua
+++ birDES.lua
@@ -1,6 +1,6 @@
 --setup
 local args = {...}
-local version = "V0.1 TESTING"
+local version = "V0.1.1 TESTING"
 local basalt = require("basalt")
 local brString = ("-----------------------------------------------------------")
 --main code
@@ -28,12 +28,12 @@ br1:setPosition(1, 5)
 br1:setText(brString)
 
 local warning = mainFrame:addLabel()
-warning:setFontSize(3)
+warning:setFontSize(2)
 warning:setText("STILL UNDER DEVELOPMENT... CHECK BACK LATER...")
 warning:setPosition((term.getSize()-warning:getSize())/2, 7)
 
 local warning2 = mainFrame:addLabel()
-warning2:setFontSize(3)
+warning2:setFontSize(2)
 warning2:setText(version)
 warning2:setPosition((term.getSize()-warning2:getSize())/2, 7)
   
